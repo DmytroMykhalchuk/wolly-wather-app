854215a07bdbdae79020cfe872b0bc87f12c00e2
Dima Mykhalchuk <dimamuchlachuk@gmail.com>
2024-03-12 09:41:44 +0200

    Added debounce hook.

--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,47 +1,59 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './styles.module.css'
-import {faMagnifyingGlassLocation} from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlassLocation } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import useDebounce from '../../utils/useDebounce';
 
-const Header=(props)=>{
-   let listLocations=[];
-   if(props.listLocations){
-      listLocations=props.listLocations.map((el,i)=>{
-         if(el.geometry.location_type==="APPROXIMATE"){
-            return( <li key={i} data-lng={el.geometry.location.lng} data-place={el.address_components[0].long_name} data-lat={el.geometry.location.lat}>{el.formatted_address}</li>)
+const Header = (props) => {
+   const [city, setCity] = useState('');
+   const debouncedCity = useDebounce(city, 800);
+   useEffect(() => {
+      if (debouncedCity) {
+         props.cityValueHandler(debouncedCity);
+      }
+   }, [debouncedCity])
+   let listLocations = [];
+   if (props.listLocations) {
+      listLocations = props.listLocations.map((el, i) => {
+         if (el.geometry.location_type === "APPROXIMATE") {
+            return (<li key={i} data-lng={el.geometry.location.lng} data-place={el.address_components[0].long_name} data-lat={el.geometry.location.lat}>{el.formatted_address}</li>)
          }
-   })}
-   listLocations=listLocations.filter(el=>el!=undefined)
-   let clazzFieldWrapper=props.isOpenField?styles.fieldWrapper+" "+styles.fieldWrapperOpened:styles.fieldWrapper;
-   return (
-   <div className={styles.wrapper}>
-      <div className={styles.header}>
-         <h1 className={styles.wrapper__title}>{props.currentLocation}</h1>
-         <button className={styles.btnSearch} onClick={props.onOpenFieldHadler}>
-            <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
-         </button>
-      </div>
-      <div className={clazzFieldWrapper}>
-         <div className={styles.box+' '+props.searchMode&&styles.boxOpened}>
+      })
+   }
 
-         {props.searchMode&&(
-            <>
-               <div style={{display:'flex',justifyContent:'center,'}}>
-                  <input type='text' placeholder='City...' 
-                  autoFocus={true}
-                  maxLength={20} value={props.city} list="locations"
-                  onChange={props.cityValueHandler}/>
-               </div>
-               <div className={styles.listWrapper}>
-                  <ul className={styles.list} onClick={props.chooseCityHandler}>
-               {listLocations.length > 0 && listLocations
-               ?listLocations: (<li>no results</li>)}
-                  </ul>
-               </div>
-               </>
-               )} 
-             </div> 
-      </div>   
-      </div>   
+   listLocations = listLocations.filter(el => el != undefined)
+   let clazzFieldWrapper = props.isOpenField ? styles.fieldWrapper + " " + styles.fieldWrapperOpened : styles.fieldWrapper;
+
+   return (
+      <div className={styles.wrapper}>
+         <div className={styles.header}>
+            <h1 className={styles.wrapper__title}>{props.currentLocation}</h1>
+            <button className={styles.btnSearch} onClick={props.onOpenFieldHadler}>
+               <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
+            </button>
+         </div>
+         <div className={clazzFieldWrapper}>
+            <div className={styles.box + ' ' + props.searchMode && styles.boxOpened}>
+
+               {props.searchMode && (
+                  <>
+                     <div style={{ display: 'flex', justifyContent: 'center,' }}>
+                        <input type='text' placeholder='City...'
+                           autoFocus={true}
+                           maxLength={20} value={city} list="locations"
+                           onChange={(e) => setCity(e.currentTarget.value)} />
+                     </div>
+                     <div className={styles.listWrapper}>
+                        <ul className={styles.list} onClick={props.chooseCityHandler}>
+                           {listLocations.length > 0 && listLocations
+                              ? listLocations : (<li>no results</li>)}
+                        </ul>
+                     </div>
+                  </>
+               )}
+            </div>
+         </div>
+      </div>
    )
 }
 
--- src/modules/header/HeaderContainer.jsx
+++ src/modules/header/HeaderContainer.jsx
@@ -4,54 +4,54 @@ import { getGeocodes, setCoordinates } from "../../redux/initializeReducer";
 import { getCurrentLocation, getListLocation } from "../../redux/initializeSelector";
 import Header from "./Header";
 
-const HeaderContainer=(props)=>{
+const HeaderContainer = (props) => {
    const [isOpenField, setIsOpenField] = useState(false)
    const [city, setCity] = useState('')
    const [searchMode, setSearchMode] = useState(false)
-   const cityValueHandler=(e)=>{
-      setCity(e.target.value);
-      props.getGeocodes(e.target.value)
+   const cityValueHandler = (city) => {
+      setCity(city);
+      props.getGeocodes(city);
 
    };
-   const onOpenFieldHadler=()=>{
+   const onOpenFieldHadler = () => {
       setIsOpenField(!isOpenField)
       setSearchMode(!isOpenField)
-      
+
    }
-   const onBlurSearchHandler=()=>{
+   const onBlurSearchHandler = () => {
       setIsOpenField(false)
       setSearchMode(false)
    }
-   const chooseCityHandler=(el)=>{
-      let lat=el.target.dataset.lat;
-      let lon=el.target.dataset.lng;
-      let place=el.target.dataset.place;
-      props.setCoordinates(lat,lon,place);
+   const chooseCityHandler = (el) => {
+      let lat = el.target.dataset.lat;
+      let lon = el.target.dataset.lng;
+      let place = el.target.dataset.place;
+      props.setCoordinates(lat, lon, place);
       onBlurSearchHandler();
    }
    return <Header currentLocation={props.currentLocation} isOpenField={isOpenField}
-   onOpenFieldHadler={onOpenFieldHadler}
-   cityValueHandler={cityValueHandler}
-   listLocations={props.listLocations}
-   searchMode={searchMode}
-   city={city}
-   chooseCityHandler={chooseCityHandler}
+      onOpenFieldHadler={onOpenFieldHadler}
+      cityValueHandler={cityValueHandler}
+      listLocations={props.listLocations}
+      searchMode={searchMode}
+      city={city}
+      chooseCityHandler={chooseCityHandler}
    />;
 }
 
-const mapStateToProps=(state)=>{
-   return{
-      currentLocation:getCurrentLocation(state),
-      listLocations:getListLocation(state)
+const mapStateToProps = (state) => {
+   return {
+      currentLocation: getCurrentLocation(state),
+      listLocations: getListLocation(state)
    }
 }
-const mapDispatchToProps=(dispatch)=>{
-   return{
-      getGeocodes:getGeocodes(dispatch),
-      setCoordinates:(lat,lon,place)=>{
-         dispatch(setCoordinates(lat,lon,place))
+const mapDispatchToProps = (dispatch) => {
+   return {
+      getGeocodes: getGeocodes(dispatch),
+      setCoordinates: (lat, lon, place) => {
+         dispatch(setCoordinates(lat, lon, place))
       }
    }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(HeaderContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderContainer);
